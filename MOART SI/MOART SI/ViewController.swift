4917126ac1d45e39c5b76d1ded520a1c87831649
yoltorsa31 <45167337+yoltorsa31@users.noreply.github.com>
2018-11-30 12:37:48 +0100

    Add files via upload

---
+++ MOART SI/MOART SI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MOART SI
+//
+//  Created by Yolanda Tortosa Sanz on 29/11/2018.
+//  Copyright © 2018 Yolanda Tortosa Sanz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MOART SI/MOART SITests/MOART_SITests.swift
@@ -0,0 +1,34 @@
+//
+//  MOART_SITests.swift
+//  MOART SITests
+//
+//  Created by Yolanda Tortosa Sanz on 29/11/2018.
+//  Copyright © 2018 Yolanda Tortosa Sanz. All rights reserved.
+//
+
+import XCTest
+@testable import MOART_SI
+
+class MOART_SITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
